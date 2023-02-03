2349c67a53aa2c17243f760fcfb207566fe9b92f
Lukas <lukasstrassel@googlemail.com>
2023-02-03 11:16:16 +0100

    feat: add diffing to helpers

--- src/test/ProtocolV3TestBase.t.sol
+++ src/test/ProtocolV3TestBase.t.sol
@@ -11,9 +11,12 @@ contract ProtocolV3TestBaseTest is ProtocolV3TestBase {
   }
 
   function testSnpashot() public {
-    this.createConfigurationSnapshot('report', AaveV3Polygon.POOL);
+    this.createConfigurationSnapshot('pre-x', AaveV3Polygon.POOL);
+    // do sth
+    // this.createConfigurationSnapshot('post-x', AaveV3Polygon.POOL);
 
-    diffReports('report_v2', 'report_v3');
+    // requires --ffi
+    // diffReports('pre-x', 'post-x');
   }
 
   // commented out as it is insanely slow with public rpcs
