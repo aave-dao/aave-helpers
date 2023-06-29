4795bbe2b564307ec287f195977bab4ae9f06af5
Lukas <lukasstrassel@googlemail.com>
2023-06-29 10:46:08 +0200

    feat: update-gov-scripts

--- tests/AaveV3ConfigEngineGauntletProposal.t.sol
+++ tests/AaveV3ConfigEngineGauntletProposal.t.sol
@@ -15,10 +15,10 @@ import {AaveV3AvalancheRatesUpdates070322} from './mocks/gauntlet-updates/AaveV3
 import {AaveV3OptimismRatesUpdates070322} from './mocks/gauntlet-updates/AaveV3OptimismRatesUpdates070322.sol';
 import {AaveV3ArbitrumRatesUpdates070322} from './mocks/gauntlet-updates/AaveV3ArbitrumRatesUpdates070322.sol';
 import {DeployEnginePolLib, DeployEngineEthLib, DeployEngineAvaLib, DeployEngineOptLib, DeployEngineArbLib} from '../scripts/AaveV3ConfigEngine.s.sol';
-import {GovHelpers, TestWithExecutor} from '../src/GovHelpers.sol';
+import {GovHelpers} from '../src/GovHelpers.sol';
 import '../src/ProtocolV3TestBase.sol';
 
-contract AaveV3PolygonConfigEngineRatesTest is ProtocolV3TestBase, TestWithExecutor {
+contract AaveV3PolygonConfigEngineRatesTest is ProtocolV3TestBase {
   using stdStorage for StdStorage;
 
   function setUp() public {
@@ -31,7 +31,7 @@ contract AaveV3PolygonConfigEngineRatesTest is ProtocolV3TestBase, TestWithExecu
 
     createConfigurationSnapshot('preTestEnginePolV3Gauntlet', AaveV3Polygon.POOL);
 
-    GovHelpers.executePayload(vm, address(payload));
+    GovHelpers.executePayload(vm, AaveGovernanceV2.POLYGON_BRIDGE_EXECUTOR, address(payload));
 
     createConfigurationSnapshot('postTestEnginePolV3Gauntlet', AaveV3Polygon.POOL);
 
@@ -39,7 +39,7 @@ contract AaveV3PolygonConfigEngineRatesTest is ProtocolV3TestBase, TestWithExecu
   }
 }
 
-contract AaveV3AvalancheConfigEngineRatesTest is ProtocolV3TestBase, TestWithExecutor {
+contract AaveV3AvalancheConfigEngineRatesTest is ProtocolV3TestBase {
   using stdStorage for StdStorage;
 
   function setUp() public {
@@ -52,7 +52,7 @@ contract AaveV3AvalancheConfigEngineRatesTest is ProtocolV3TestBase, TestWithExe
 
     createConfigurationSnapshot('preTestEngineAvaV3Gauntlet', AaveV3Avalanche.POOL);
 
-    GovHelpers.executePayload(vm, address(payload), 0xa35b76E4935449E33C56aB24b23fcd3246f13470); // Aave Avalanche's Guardian
+    GovHelpers.executePayload(vm, 0xa35b76E4935449E33C56aB24b23fcd3246f13470, address(payload)); // Aave Avalanche's Guardian
 
     createConfigurationSnapshot('postTestEngineAvaV3Gauntlet', AaveV3Avalanche.POOL);
 
@@ -73,7 +73,7 @@ contract AaveV3OptimismConfigEngineRatesTest is ProtocolV3TestBase {
 
     createConfigurationSnapshot('preTestEngineOptV3Gauntlet', AaveV3Optimism.POOL);
 
-    GovHelpers.executePayload(vm, address(payload));
+    GovHelpers.executePayload(vm, AaveGovernanceV2.OPTIMISM_BRIDGE_EXECUTOR, address(payload));
 
     createConfigurationSnapshot('postTestEngineOptV3Gauntlet', AaveV3Optimism.POOL);
 
@@ -94,7 +94,7 @@ contract AaveV3ArbitrumConfigEngineRatesTest is ProtocolV3TestBase {
 
     createConfigurationSnapshot('preTestEngineArbV3Gauntlet', AaveV3Arbitrum.POOL);
 
-    GovHelpers.executePayload(vm, address(payload));
+    GovHelpers.executePayload(vm, AaveGovernanceV2.ARBITRUM_BRIDGE_EXECUTOR, address(payload));
 
     createConfigurationSnapshot('postTestEngineArbV3Gauntlet', AaveV3Arbitrum.POOL);
 
