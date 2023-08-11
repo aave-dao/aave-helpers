6fce3d93cf778c9cc7e19156456c40de26d73bce
Rustem Kurmaev <rustboyar@gmail.com>
2023-08-11 14:15:28 +0200

    update comment

--- src/v3-config-engine/AaveV3PayloadBasenet.sol
+++ src/v3-config-engine/AaveV3PayloadBasenet.sol
@@ -5,7 +5,7 @@ import {AaveV3Basenet} from 'aave-address-book/AaveV3Basenet.sol';
 import './AaveV3PayloadBase.sol';
 
 /**
- * @dev Base smart contract for an Aave v3.0.1 (compatible with 3.0.0) listing on v3 Basenet.
+ * @dev Base smart contract for an Aave v3.0.2 (compatible with 3.0.0) listing on v3 Basenet.
  * @author BGD Labs
  */
 abstract contract AaveV3PayloadBasenet is AaveV3PayloadBase(IEngine(AaveV3Basenet.LISTING_ENGINE)) {
