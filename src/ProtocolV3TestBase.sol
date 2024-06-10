ed341a97045eb98675eb14efef3089bc2b2663f0
Harsh Pandey <harshsatishpandey@gmail.com>
2024-06-10 22:07:17 +0530

    fix: configChangePlausibilityTest for GHO

--- src/ProtocolV3TestBase.sol
+++ src/ProtocolV3TestBase.sol
@@ -124,18 +124,22 @@ contract ProtocolV3TestBase is CommonTestBase {
           require(configAfter[i].borrowingEnabled, 'PL_BORROW_CAP_BORROW_DISABLED');
         }
       } else {
-        // at least newly listed assets should never have a supply cap exceeding total supply
-        uint256 totalSupply = IERC20(configAfter[i].underlying).totalSupply();
-        require(
-          configAfter[i].supplyCap / 1e2 <=
-            totalSupply / IERC20Metadata(configAfter[i].underlying).decimals(),
-          'PL_SUPPLY_CAP_GT_TOTAL_SUPPLY'
-        );
+        if (
+          keccak256(bytes(configAfter[i].symbol)) != keccak256(bytes('GHO')) // GHO is the exclusion from the rule
+        ) {
+          // at least newly listed assets should never have a supply cap exceeding total supply
+          uint256 totalSupply = IERC20(configAfter[i].underlying).totalSupply();
+          require(
+            configAfter[i].supplyCap / 1e2 <=
+              totalSupply / IERC20Metadata(configAfter[i].underlying).decimals(),
+            'PL_SUPPLY_CAP_GT_TOTAL_SUPPLY'
+          );
+        }
       }
       // borrow cap should never exceed supply cap
       if (
         configAfter[i].borrowCap != 0 &&
-        configAfter[i].underlying != AaveV3EthereumAssets.GHO_UNDERLYING // GHO is the exclusion from the rule
+        keccak256(bytes(configAfter[i].symbol)) != keccak256(bytes('GHO')) // GHO is the exclusion from the rule
       ) {
         console.log(configAfter[i].underlying);
         require(configAfter[i].borrowCap <= configAfter[i].supplyCap, 'PL_SUPPLY_LT_BORROW');
