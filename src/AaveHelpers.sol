b453ef381a59367aa303615f1dd4dfc8ee02393b
Harsh Pandey <harshsatishpandey@gmail.com>
2024-09-10 18:55:59 +0530

    chore: remove old gov helpers

--- src/AaveHelpers.sol
+++ src/AaveHelpers.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity >=0.7.5 <0.9.0;
 
-import {GovHelpers, AaveGovernanceV2} from './GovHelpers.sol';
+import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {ProxyHelpers} from './ProxyHelpers.sol';
 import {ProtocolV3TestBase} from './ProtocolV3TestBase.sol';
