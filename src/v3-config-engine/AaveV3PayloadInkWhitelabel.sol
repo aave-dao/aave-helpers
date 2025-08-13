f81efa5dfdb0418f5363d5ce5fdfc14b8d1c8f7d
Harsh Pandey <harshsatishpandey@gmail.com>
2025-08-13 16:20:16 +0400

    chore: fix lint

--- src/v3-config-engine/AaveV3PayloadInkWhitelabel.sol
+++ src/v3-config-engine/AaveV3PayloadInkWhitelabel.sol
@@ -8,7 +8,9 @@ import 'aave-v3-origin/contracts/extensions/v3-config-engine/AaveV3Payload.sol';
  * @dev Base smart contract for an Aave v3.5.0 listing on v3 Ink Whitelabel.
  * @author BGD Labs
  */
-abstract contract AaveV3PayloadInkWhitelabel is AaveV3Payload(IEngine(AaveV3InkWhitelabel.CONFIG_ENGINE)) {
+abstract contract AaveV3PayloadInkWhitelabel is
+  AaveV3Payload(IEngine(AaveV3InkWhitelabel.CONFIG_ENGINE))
+{
   function getPoolContext() public pure override returns (IEngine.PoolContext memory) {
     return IEngine.PoolContext({networkName: 'InkWhitelabel', networkAbbreviation: 'InkWl'});
   }
