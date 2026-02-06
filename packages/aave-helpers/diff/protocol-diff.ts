37bbd814896e1397581c7caba02df61e00a1768e
Lukas <lukasstrassel@googlemail.com>
2026-02-06 22:55:49 +0100

    fix: start inlining tooling

---
+++ packages/aave-helpers/diff/protocol-diff.ts
@@ -0,0 +1,198 @@
+import { writeFileSync } from 'fs';
+import { diff } from './diff';
+import { renderEmodeDiff } from './emode';
+import { renderReserve, renderReserveDiff } from './reserve';
+import { type AaveV3Snapshot } from './snapshot-types';
+import { renderStrategy, renderStrategyDiff } from './strategy';
+import { diffRawStorage } from './raw-storage-diff';
+import {
+  BlockscoutStyleSourceCode,
+  getSourceCode,
+  parseBlockscoutStyleSourceCode,
+  parseEtherscanStyleSourceCode,
+  StandardJsonInput,
+  diffCode,
+} from '@bgd-labs/toolbox';
+
+function hasDiff(input: Record<string, any>): boolean {
+  if (!input) return false;
+  return !!Object.keys(input).find(
+    (key) =>
+      typeof input[key as keyof typeof input] === 'object' &&
+      input[key as keyof typeof input] !== null &&
+      (input[key as keyof typeof input].hasOwnProperty('from') ||
+        input[key as keyof typeof input].hasOwnProperty('to'))
+  );
+}
+
+export async function diffReports<A extends AaveV3Snapshot, B extends AaveV3Snapshot>(
+  pre: A,
+  post: B
+) {
+  const chainId = pre.chainId;
+  // if raw is present, it already is a diff, so no need to diff it
+  let raw;
+  if (post.raw) {
+    raw = { ...post.raw };
+    delete post.raw;
+  }
+  let logs;
+  if (post.logs) {
+    logs = { ...post.logs };
+    delete post.logs;
+  }
+  const diffResult = diff(pre, post);
+  const diffResultWithoutUnchanged = diff(pre, post, true);
+  if (raw) {
+    diffResultWithoutUnchanged.raw = raw as any;
+  }
+  if (logs) {
+    diffResultWithoutUnchanged.logs = logs as any;
+  }
+
+  // create report
+  let content = '';
+  if (diffResult.reserves) {
+    const reservesAdded = Object.keys(diffResult.reserves)
+      .map((reserveKey) => {
+        // to being present on reserve level % trueish means reserve was added
+        if ((diffResult.reserves[reserveKey] as any).to) {
+          let report = renderReserve((diffResult.reserves[reserveKey] as any).to, chainId);
+          report += renderStrategy(post.strategies[reserveKey]);
+          report += `| interestRate | ![ir](${getStrategyImageUrl(post.strategies[reserveKey])}) |\n`;
+
+          return report;
+        }
+      })
+      .filter((i) => i);
+    const reservesRemoved = Object.keys(diffResult.reserves)
+      .map((reserveKey) => {
+        // from being present on reserve level % trueish means reserve was removed
+        if ((diffResult.reserves[reserveKey] as any).from) {
+          return renderReserve((diffResult.reserves[reserveKey] as any).from, chainId);
+        }
+      })
+      .filter((i) => i);
+    const reservesAltered = Object.keys(diffResult.reserves)
+      .map((reserveKey) => {
+        // "from" being present on reserses key means reserve was removed
+        if (!(diffResult.reserves[reserveKey] as any).hasOwnProperty('from')) {
+          const hasChangedReserveProperties = hasDiff(diffResult.reserves[reserveKey]);
+          const preIr = getStrategyImageUrl(pre.strategies[reserveKey]);
+          const postIr = getStrategyImageUrl(post.strategies[reserveKey]);
+          const hasChangedIr = preIr !== postIr;
+          if (!hasChangedReserveProperties && !hasChangedIr) return;
+          // diff reserve
+          let report = renderReserveDiff(diffResult.reserves[reserveKey] as any, chainId);
+          // diff irs
+          if (hasChangedIr) {
+            report += renderStrategyDiff(
+              diff(pre.strategies[reserveKey], post.strategies[reserveKey]) as any
+            );
+            report += `| interestRate | ![before](${preIr}) | ![after](${postIr}) |`;
+          }
+
+          return report;
+        }
+      })
+      .filter((i) => i);
+    if (reservesAdded.length || reservesRemoved.length || reservesAltered.length) {
+      content += '## Reserve changes\n\n';
+      if (reservesAdded.length) {
+        content += `### ${reservesAdded.length > 1 ? 'Reserve' : 'Reserves'} added\n\n`;
+        content += reservesAdded.join('\n\n');
+        content += '\n\n';
+      }
+
+      if (reservesAltered.length) {
+        content += `### ${reservesAltered.length > 1 ? 'Reserve' : 'Reserves'} altered\n\n`;
+        content += reservesAltered.join('\n\n');
+        content += '\n\n';
+      }
+
+      if (reservesRemoved.length) {
+        content += `### ${reservesRemoved.length > 1 ? 'Reserve' : 'Reserves'} removed\n\n`;
+        content += reservesRemoved.join('\n\n');
+        content += '\n\n';
+      }
+    }
+  }
+
+  if (diffResultWithoutUnchanged.eModes) {
+    content += '## Emodes changed\n\n';
+    for (const eMode of Object.keys(diffResult.eModes)) {
+      const hasChanges = hasDiff(diffResult.eModes?.[eMode]);
+      content += `### EMode: ${post.eModes[eMode].label}(id: ${post.eModes[eMode].eModeCategory})\n\n`;
+      if (hasChanges) {
+        content += renderEmodeDiff(
+          diff(pre.eModes[eMode] || {}, post.eModes[eMode] || {}) as any,
+          pre,
+          post
+        );
+      } else if (!pre.eModes[eMode] || !post.eModes[eMode]) {
+        content += renderEmodeDiff(
+          diff(pre.eModes[eMode] || {}, post.eModes[eMode] || {}) as any,
+          pre,
+          post
+        );
+      }
+      content += '\n\n';
+    }
+  }
+
+  if (raw) {
+    diffRawStorage(pre.chainId, raw);
+  }
+
+  try {
+    if (diffResultWithoutUnchanged.poolConfig) {
+      for (const key of Object.keys(diffResult.poolConfig)) {
+        try {
+          if (
+            typeof (diffResult as any).poolConfig[key] === 'object' &&
+            (diffResult as any).poolConfig[key].hasOwnProperty('from')
+          ) {
+            const fromAddress = (diffResult as any).poolConfig[key].from;
+            const toAddress = (diffResult as any).poolConfig[key].to;
+            const sources = await Promise.all([
+              getSourceCode({
+                chainId: Number(pre.chainId),
+                address: fromAddress,
+                apiKey: process.env.ETHERSCAN_API_KEY,
+                apiUrl: process.env.EXPLORER_PROXY,
+              }),
+              getSourceCode({
+                chainId: Number(pre.chainId),
+                address: toAddress,
+                apiKey: process.env.ETHERSCAN_API_KEY,
+                apiUrl: process.env.EXPLORER_PROXY,
+              }),
+            ]);
+            const source1: StandardJsonInput = (sources[0] as BlockscoutStyleSourceCode)
+              .AdditionalSources
+              ? parseBlockscoutStyleSourceCode(sources[0] as BlockscoutStyleSourceCode)
+              : parseEtherscanStyleSourceCode(sources[0].SourceCode);
+            const source2: StandardJsonInput = (sources[0] as BlockscoutStyleSourceCode)
+              .AdditionalSources
+              ? parseBlockscoutStyleSourceCode(sources[1] as BlockscoutStyleSourceCode)
+              : parseEtherscanStyleSourceCode(sources[1].SourceCode);
+            const diff = await diffCode(source1, source2);
+            const flat = Object.keys(diff).reduce((acc, key) => {
+              acc += diff[key];
+              return acc;
+            }, '');
+            writeFileSync(`./diffs/${pre.chainId}_${key}_${fromAddress}_${toAddress}.diff`, flat);
+          }
+        } catch (e) {
+          console.info('error diffing the code');
+          console.info(
+            'this can happen if etherscan is not available, the code is not verified or not deployed yet'
+          );
+        }
+      }
+    }
+  } catch (e) {}
+
+  content += `## Raw diff\n\n\`\`\`json\n${JSON.stringify(diffResultWithoutUnchanged, null, 2)}\n\`\`\``;
+  return content;
+}
---
+++ packages/aave-helpers/diff/snapshot-types.ts
@@ -0,0 +1,182 @@
+import { Address, Hex } from 'viem';
+import { zksync } from 'viem/chains';
+import { z } from 'zod';
+
+export const aaveV3ConfigSchema = z.object({
+  oracle: z.string(),
+  pool: z.string(),
+  poolAddressesProvider: z.string(),
+  poolConfigurator: z.string(),
+  poolConfiguratorImpl: z.string(),
+  poolImpl: z.string(),
+  protocolDataProvider: z.string(),
+});
+
+export type AaveV3Config = z.infer<typeof aaveV3ConfigSchema>;
+
+export const aaveV3ReserveSchema = z.object({
+  id: z.number(),
+  isBorrowableInIsolation: z.boolean(),
+  borrowCap: z.number(),
+  liquidationBonus: z.number(),
+  underlying: z.string(),
+  isFrozen: z.boolean(),
+  stableDebtToken: z.string(),
+  variableDebtToken: z.string(),
+  reserveFactor: z.number(),
+  liquidationProtocolFee: z.number(),
+  usageAsCollateralEnabled: z.boolean(),
+  ltv: z.number(),
+  supplyCap: z.number(),
+  debtCeiling: z.number(),
+  borrowingEnabled: z.boolean(),
+  isActive: z.boolean(),
+  eModeCategory: z.number(),
+  symbol: z.string(),
+  stableBorrowRateEnabled: z.boolean(),
+  isFlashloanable: z.boolean(),
+  aToken: z.string(),
+  liquidationThreshold: z.number(),
+  aTokenImpl: z.string(),
+  stableDebtTokenImpl: z.string(),
+  interestRateStrategy: z.string(),
+  variableDebtTokenImpl: z.string(),
+  oracleLatestAnswer: z.string(),
+  oracle: z.string(),
+  oracleDecimals: z.number(),
+  oracleName: z.string(),
+  oracleDescription: z.string(),
+  decimals: z.number(),
+  isSiloed: z.boolean(),
+  liquidityIndex: z.number(),
+  variableBorrowIndex: z.number(),
+  currentLiquidityRate: z.number(),
+  currentVariableBorrowRate: z.number(),
+  aTokenUnderlyingBalance: z.string(),
+  virtualAccountingActive: z.boolean(),
+  virtualBalance: z.string(),
+});
+
+export type AaveV3Reserve = z.infer<typeof aaveV3ReserveSchema>;
+
+export const aaveV3StrategySchema = z.object({
+  address: z.string(),
+  baseVariableBorrowRate: z.string(),
+  optimalUsageRatio: z.string(),
+  variableRateSlope1: z.string(),
+  variableRateSlope2: z.string(),
+  maxVariableBorrowRate: z.string(),
+});
+
+export type AaveV3Strategy = z.infer<typeof aaveV3StrategySchema>;
+
+export const aaveV3EmodeSchema = z.object({
+  eModeCategory: z.number(),
+  liquidationBonus: z.number(),
+  label: z.string(),
+  liquidationThreshold: z.number(),
+  priceSource: z.string(),
+  ltv: z.number(),
+  borrowableBitmap: z.string(),
+  collateralBitmap: z.string(),
+});
+
+export type AaveV3Emode = z.infer<typeof aaveV3EmodeSchema>;
+
+export const CHAIN_ID = {
+  MAINNET: 1,
+  OPTIMISM: 10,
+  POLYGON: 137,
+  FANTOM: 250,
+  ARBITRUM: 42161,
+  AVALANCHE: 43114,
+  METIS: 1088,
+  BASE: 8453,
+  SCROLL: 534352,
+  BNB: 56,
+  GNOSIS: 100,
+  CELO: 42220,
+  ZKSYNC: zksync.id,
+} as const;
+
+const zodChainId = z.nativeEnum(CHAIN_ID);
+
+export type CHAIN_ID = z.infer<typeof zodChainId>;
+
+export const slotDiff = z.object({
+  previousValue: z.string() as z.ZodType<Hex>,
+  newValue: z.string() as z.ZodType<Hex>,
+  label: z.string().optional(), // does not initially exist, but we might want to inject information here
+});
+
+export const rawStorageSchema = z.record(
+  z.string() as z.ZodType<Address>,
+  z.object({
+    label: z.string().nullable(),
+    balanceDiff: z.string().nullable(),
+    stateDiff: z.record(z.string(), slotDiff),
+  })
+);
+
+export type RawStorage = z.infer<typeof rawStorageSchema>;
+
+export type SlotDiff = z.infer<typeof slotDiff>;
+
+export const aaveV3SnapshotSchema = z.object({
+  reserves: z.record(z.string(), aaveV3ReserveSchema),
+  strategies: z.record(z.string(), aaveV3StrategySchema),
+  eModes: z.record(z.string(), aaveV3EmodeSchema),
+  poolConfig: aaveV3ConfigSchema,
+  chainId: zodChainId,
+  raw: rawStorageSchema.optional(),
+});
+
+export const aDIReceiverConfigSchema = z.object({
+  requiredConfirmations: z.number(),
+  validityTimestamp: z.number(),
+});
+export const aDIAdapterSchema = z.record(z.string(), z.string());
+
+export const aDISnapshotSchema = z.object({
+  receiverConfigs: z.record(zodChainId, aDIReceiverConfigSchema),
+  forwarderAdaptersByChain: z.record(zodChainId, aDIAdapterSchema),
+  receiverAdaptersByChain: z.record(zodChainId, aDIAdapterSchema),
+  chainId: zodChainId,
+});
+
+export type AaveV3Snapshot = z.infer<typeof aaveV3SnapshotSchema>;
+export type ADISnapshot = z.infer<typeof aDISnapshotSchema>;
+
+export const capoPriceSchema = z.object({
+  referencePrice: z.number(),
+  sourcePrice: z.number(),
+  timestamp: z.number(),
+  dayToDayGrowth: z.number(),
+  smoothedGrowth: z.number(),
+});
+
+export const capoSnapshotSchema = z.object({
+  decimals: z.number(),
+  reference: z.string(),
+  source: z.string(),
+  maxYearlyGrowthPercent: z.number(),
+  minSnapshotDelay: z.number(),
+  prices: z.record(z.string(), capoPriceSchema),
+});
+
+export type CapoSnapshot = z.infer<typeof capoSnapshotSchema>;
+
+export const capoSvrPriceSchema = z.object({
+  regularAdapterPrice: z.number(),
+  svrAdapterPrice: z.number(),
+  timestamp: z.number(),
+});
+
+export const capoSvrSnapshotSchema = z.object({
+  sourceName: z.string(),
+  decimals: z.number(),
+  minSnapshotDelay: z.number(),
+  comparative: z.record(z.string(), capoSvrPriceSchema),
+});
+
+export type CapoSvrSnapshot = z.infer<typeof capoSvrSnapshotSchema>;
