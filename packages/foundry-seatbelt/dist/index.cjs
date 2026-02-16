c44d69121ba3b8b391af5535c44f4d765f3fec4a
Lukas <lukasstrassel@googlemail.com>
2026-02-16 13:54:59 +0100

    fix: try mermaid

--- packages/foundry-seatbelt/dist/index.cjs
+++
@@ -1,7 +0,0 @@
-Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-const require_protocol_diff = require('./protocol-diff-BpwrHdvJ.cjs');
-
-exports.diff = require_protocol_diff.diff;
-exports.diffSnapshots = require_protocol_diff.diffSnapshots;
-exports.hasChanges = require_protocol_diff.hasChanges;
-exports.isChange = require_protocol_diff.isChange;
--- packages/foundry-seatbelt/dist/index.mjs
+++
@@ -1,3 +0,0 @@
-import { i as isChange, n as diff, r as hasChanges, t as diffSnapshots } from "./protocol-diff-OQplGnCt.mjs";
-
-export { diff, diffSnapshots, hasChanges, isChange };
