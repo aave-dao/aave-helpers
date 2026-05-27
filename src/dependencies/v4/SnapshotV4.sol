418a645402633b95fd2612f764842ce64439ca10
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2026-05-27 12:00:35 +0530

    fix: v4 snapshot

--- src/dependencies/v4/SnapshotV4.sol
+++ src/dependencies/v4/SnapshotV4.sol
@@ -41,7 +41,7 @@ abstract contract SnapshotV4 is Helpers {
 
     string[] memory inputs = new string[](7);
     inputs[0] = 'npx';
-    inputs[1] = '@aave-dao/aave-helpers-js@^1.0.1';
+    inputs[1] = '@aave-dao/aave-helpers-js@^1.2.1';
     inputs[2] = 'diff-v4-snapshots';
     inputs[3] = beforePath;
     inputs[4] = afterPath;
