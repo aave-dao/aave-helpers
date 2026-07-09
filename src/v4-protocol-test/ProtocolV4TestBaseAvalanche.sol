d740703ec2d152dee8aa61a5741d68c1b240b220
dhairya <55102840+DhairyaSethi@users.noreply.github.com>
2026-07-09 17:09:54 +0530

    feat: avalanche v4 (#764)
    * feat: avalanche v4
    
    * feat: make them  virtual
    
    * feat: update address book package

--- src/v4-protocol-test/ProtocolV4TestBaseAvalanche.sol
+++ src/v4-protocol-test/ProtocolV4TestBaseAvalanche.sol
@@ -2,19 +2,19 @@
 pragma solidity ^0.8.0;
 
 import {ISpoke, IHub, ITokenizationSpoke, ISpokeConfigurator, PositionManagers} from 'aave-address-book/AaveV4.sol';
-import {AaveV4Ethereum, AaveV4EthereumGetters} from 'aave-address-book/AaveV4Ethereum.sol';
+import {AaveV4Avalanche, AaveV4AvalancheGetters} from 'aave-address-book/AaveV4Avalanche.sol';
 import {ProtocolV4TestBase} from 'src/ProtocolV4TestBase.sol';
 
-/// @title ProtocolV4TestBaseEthereum
-/// @notice Ethereum binding of the chain-agnostic ProtocolV4TestBase, sourcing every
+/// @title ProtocolV4TestBaseAvalanche
+/// @notice Avalanche binding of the chain-agnostic ProtocolV4TestBase, sourcing every
 ///         network entity from the aave-address-book getters.
-contract ProtocolV4TestBaseEthereum is ProtocolV4TestBase {
+contract ProtocolV4TestBaseAvalanche is ProtocolV4TestBase {
   function _getHubs() internal view virtual override returns (IHub[] memory) {
-    return AaveV4EthereumGetters.getAllHubs();
+    return AaveV4AvalancheGetters.getAllHubs();
   }
 
   function _getSpokes() internal view virtual override returns (ISpoke[] memory) {
-    return AaveV4EthereumGetters.getAllSpokes();
+    return AaveV4AvalancheGetters.getAllSpokes();
   }
 
   function _getTokenizationSpokes()
@@ -24,18 +24,18 @@ contract ProtocolV4TestBaseEthereum is ProtocolV4TestBase {
     override
     returns (ITokenizationSpoke[] memory)
   {
-    return AaveV4EthereumGetters.getAllTokenizationSpokes();
+    return AaveV4AvalancheGetters.getAllTokenizationSpokes();
   }
 
   function _getPositionManagers() internal view virtual override returns (PositionManagers memory) {
-    return AaveV4EthereumGetters.getPositionManagers();
+    return AaveV4AvalancheGetters.getPositionManagers();
   }
 
   function _accessManager() internal view virtual override returns (address) {
-    return address(AaveV4Ethereum.ACCESS_MANAGER);
+    return address(AaveV4Avalanche.ACCESS_MANAGER);
   }
 
   function _spokeConfigurator() internal view virtual override returns (ISpokeConfigurator) {
-    return AaveV4Ethereum.SPOKE_CONFIGURATOR;
+    return AaveV4Avalanche.SPOKE_CONFIGURATOR;
   }
 }
