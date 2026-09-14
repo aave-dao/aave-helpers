8f50a977397af02afeba8e1e9adfd1d26e70f6f0
Alberto <11707683+AlbertoCentonze@users.noreply.github.com>
2026-09-14 17:19:48 +0200

    fix: remove obsolete zkSync reserve fields (#806)

--- zksync/src/ProtocolV3TestBase.sol
+++ zksync/src/ProtocolV3TestBase.sol
@@ -464,12 +464,8 @@ contract ProtocolV3TestBase is RawProtocolV3TestBase, CommonTestBase, DiffUtils 
       if (
         // not frozen etc
         // usable as collateral
-        // not isolated asset as we can only borrow stablecoins against it
         // ltv is not 0
-        _includeInE2e(configs[i]) &&
-        configs[i].usageAsCollateralEnabled &&
-        configs[i].debtCeiling == 0 &&
-        configs[i].ltv != 0
+        _includeInE2e(configs[i]) && configs[i].usageAsCollateralEnabled && configs[i].ltv != 0
       ) return configs[i];
     }
     revert('ERROR: No usable collateral found');
